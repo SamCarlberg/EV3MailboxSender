aeb901a499f18b48b34e49026b27e3728756691d
Sam Carlberg <slcarlberg@wpi.edu>
2014-07-13 13:17:39 -0400

    Changed communications library
    The old one wasn't fun, so switched to EV3MessengerLib. And it works.

--- Program.cs
+++ Program.cs
@@ -5,121 +5,172 @@ using System.Collections.Generic;
 using System.IO;
 using MonoBrick.EV3;
 using MonoBrick;
+using EV3MessengerLib;
 
 public static class Program
 {
-	static Brick<Sensor, Sensor, Sensor, Sensor> ev3 = null;
+    //static Brick<Sensor, Sensor, Sensor, Sensor> ev3 = null;
+    private static EV3Messenger messenger = new EV3Messenger();
 
-	static void Main (string[] args)
-	{
-		Console.OpenStandardOutput ();
-		Console.OpenStandardInput ();
-		Console.WriteLine ("Starting C# backend");
-		String input = "";
-		connect ();
-		while (true) {
-			input = Console.In.ReadLine ();
-			if (input.Equals ("exit") || input.Equals ("q")) {
-				Console.WriteLine ("Exiting C# backend.");
-				break;
-			} else if (input.Equals ("connect")) {
-				connect ();
-				continue;
-			} else {
-				String[] cmd = input.Split (':');
-				if (cmd.Length > 2) {
-					Console.WriteLine ("Invalid arg: " + input);
-					continue;
-				}
+    static void Main(string[] args)
+    {
+        Console.OpenStandardOutput();
+        Console.OpenStandardInput();
+        Console.WriteLine("Starting C# backend");
+        String input = "";
+        connect();
+        while (true)
+        {
+            input = Console.In.ReadLine();
+            if (input.Equals("exit") || input.Equals("q"))
+            {
+                Console.WriteLine("Exiting C# backend.");
+                break;
+            }
+            else if (input.Equals("connect"))
+            {
+                connect();
+                continue;
+            }
+            else
+            {
+                String[] cmd = input.Split(':');
+                if (cmd.Length > 2)
+                {
+                    Console.WriteLine("Invalid arg: " + input);
+                    continue;
+                }
 
-				try {
-					if (cmd.Length == 2) {
-						SendInt ("command", int.Parse (cmd [0]));
-						SendFloat ("operand", float.Parse (cmd [1]));
-					} else {
-						SendInt ("command", int.Parse (input));
-					}
-				} catch (FormatException) {
-					Console.WriteLine ("Invalid arg");
-				}
-			}
-		}
-	}
+                try
+                {
+                    SendMessage("command", int.Parse(cmd[0]));
+                    if (cmd.Length == 2)
+                    {
+                        SendMessage("operand", float.Parse(cmd[1]));
+                    }
+                    else
+                    {
+                        SendMessage("operand", 0);
+                    }
+                    while (messenger.ReadMessage() == null)
+                    {
+                        System.Threading.Thread.Sleep(50);
+                    }
+                    Console.WriteLine("Got response");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid arg");
+                }
+            }
+        }
+        messenger.Disconnect();
+    }
 
-	private static void SendInt (String mailbox, int value)
-	{
-		Console.WriteLine ("Sending command #" + value);
-		Console.WriteLine ("command bytes:" + BitConverter.ToString (BitConverter.GetBytes (value)));
-		if (ev3 == null)
-			return;
-		ev3.Mailbox.Send (mailbox, BitConverter.GetBytes (value));
-	}
+    private static void SendMessage(String mailbox, String message)
+    {
+        Console.WriteLine("Sending message " + message + " to mailbox " + mailbox);
+        if (messenger.IsConnected)
+        {
+            messenger.SendMessage(mailbox, message);
+        }
+    }
 
-	private static void SendFloat (String mailbox, float value)
-	{
-		Console.WriteLine ("\tOperand " + value);
-		Console.WriteLine ("operand bytes:" + BitConverter.ToString (BitConverter.GetBytes (value)));
-		if (ev3 == null)
-			return;
-		ev3.Mailbox.Send (mailbox, BitConverter.GetBytes (value));
-	}
+    private static void SendMessage(String mailbox, int message)
+    {
+        Console.WriteLine("Sending message " + message + " to mailbox " + mailbox);
+        if (messenger.IsConnected)
+        {
+            messenger.SendMessage(mailbox, message);
+        }
+    }
+    private static void SendMessage(String mailbox, float message)
+    {
+        Console.WriteLine("Sending message " + message + " to mailbox " + mailbox);
+        if (messenger.IsConnected)
+        {
+            messenger.SendMessage(mailbox, message);
+        }
+    }
 
-	// Make sure you have a reference to System.Management or this won't compile
-	//
-	// Query code from https://32feet.codeplex.com/
-	// Modified by Xander Soldaat
-	static List<String> scan ()
-	{
-		// The WMI query that makes it all happen
-		const string QueryString = "SELECT Caption,PNPDeviceID FROM Win32_PnPEntity " +
-			"WHERE ConfigManagerErrorCode = 0 AND " +
-			"Caption LIKE 'Standard Serial over Bluetooth link (COM%' AND " +
-			"PNPDeviceID LIKE '%&001653%'";
+    private static void SendMessage(String mailbox, bool message)
+    {
+        Console.WriteLine("Sending message " + message + " to mailbox " + mailbox);
+        if (messenger.IsConnected)
+        {
+            messenger.SendMessage(mailbox, message);
+        }
+    }
 
-		SelectQuery WMIquery = new SelectQuery (QueryString);
-		ManagementObjectSearcher WMIqueryResults = new ManagementObjectSearcher (WMIquery);
-		if (WMIqueryResults != null) {
-			List<String> comPorts = new List<String> ();
-			ManagementObjectCollection results = WMIqueryResults.Get ();
-			if (results.Count == 0) {
-				Console.WriteLine ("No NXTs found");
-				return comPorts;
-			}
-			Console.WriteLine ("The following NXTs were found on your system:");
-			foreach (ManagementObject mo in results) {
-				object captionObject = mo.GetPropertyValue ("Caption");
-				object pnpIdObject = mo.GetPropertyValue ("PNPDeviceID");
+    // Make sure you have a reference to System.Management or this won't compile
+    //
+    // Query code from https://32feet.codeplex.com/
+    // Modified by Xander Soldaat
+    private static List<String> scan()
+    {
+        // The WMI query that makes it all happen
+        const string QueryString = "SELECT Caption,PNPDeviceID FROM Win32_PnPEntity " +
+            "WHERE ConfigManagerErrorCode = 0 AND " +
+            "Caption LIKE 'Standard Serial over Bluetooth link (COM%' AND " +
+            "PNPDeviceID LIKE '%&001653%'";
 
-				// Get the COM port name out of the Caption, requires a little search and replacing.
-				string caption = captionObject.ToString ();
-				string comPort = caption.Substring (caption.LastIndexOf ("(COM")).
-                                    Replace ("(", string.Empty).Replace (")", string.Empty);
+        SelectQuery WMIquery = new SelectQuery(QueryString);
+        ManagementObjectSearcher WMIqueryResults = new ManagementObjectSearcher(WMIquery);
+        if (WMIqueryResults != null)
+        {
+            List<String> comPorts = new List<String>();
+            ManagementObjectCollection results = WMIqueryResults.Get();
+            if (results.Count == 0)
+            {
+                Console.WriteLine("No NXTs found");
+                return comPorts;
+            }
+            Console.WriteLine("The following NXTs were found on your system:");
+            foreach (ManagementObject mo in results)
+            {
+                object captionObject = mo.GetPropertyValue("Caption");
+                object pnpIdObject = mo.GetPropertyValue("PNPDeviceID");
 
-				// Extract the BT address from the PNPObjectID property
-				string BTaddress = pnpIdObject.ToString ().Split ('&') [4].Substring (0, 12);
-				comPorts.Add (comPort);
-			}
-			return comPorts;
-		} else {
-			Console.WriteLine ("Error executing query");
-			return null;
-		}
-	}
+                // Get the COM port name out of the Caption, requires a little search and replacing.
+                string caption = captionObject.ToString();
+                string comPort = caption.Substring(caption.LastIndexOf("(COM")).
+                                    Replace("(", string.Empty).Replace(")", string.Empty);
 
-	static void connect ()
-	{
-		foreach (String port in scan()) {
-			try {
-				ev3 = new Brick<Sensor, Sensor, Sensor, Sensor> (port);
-				Console.WriteLine ("\tTrying to connect to NXT on " + port);
-				ev3.Connection.Open ();
-			} catch (ConnectionException e) {
-				Console.WriteLine ("\tCould not connect on " + port + ": " + e.Message);
-				continue;
-			}
-			Console.WriteLine ("connected");
-			return;
-		}
-		Console.WriteLine ("could not connect");
-	}
+                // Extract the BT address from the PNPObjectID property
+                string BTaddress = pnpIdObject.ToString().Split('&')[4].Substring(0, 12);
+                comPorts.Add(comPort);
+                Console.WriteLine("\t" + comPort);
+            }
+            return comPorts;
+        }
+        else
+        {
+            Console.WriteLine("Error executing query");
+            return null;
+        }
+    }
+
+    private static void connect()
+    {
+        foreach (String port in scan())
+        {
+            //ev3 = new Brick<Sensor, Sensor, Sensor, Sensor>(port);
+            Console.WriteLine("\tTrying to connect to NXT on " + port);
+            //ev3.Connection.Open();
+            try
+            {
+                if (messenger.Connect(port))
+                {
+                    Console.WriteLine("connected");
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                break;
+            }
+        }
+        Console.WriteLine("could not connect");
+    }
 }
